bbe67ecf7d2518a023930af2d3c9701d4c6b83a2
Kyle Treptow <treptow@apollointeractive.com>
2020-10-06 21:28:28 -0700

    blocking out sections

--- assets/js/App.vue
+++ assets/js/App.vue
@@ -1,19 +1,42 @@
 <template lang="pug">
   .app
-    aside
-      p Aside
-    main
-      h1 PUG WORKS
-      p Welcome, {{ name}}
+    aside.aside
+      .unit
+        p
+          b General
+        p Race, Subraces
+        p Class, Archetypes
+        p Portrait
+        p Inspiration
+      .unit XP Bar
+      .unit
+        p
+          b Abilities
+        ul
+          li Strength
+          li Dexterity
+          li Constitution
+          li Intelligence
+          li Wisdom
+          li Charisma
+    main.main
+      .unit
+        p
+          b Combat
+        p Health, AC, Initiative, Dice Roller, etc.
+      .unit
+        h1 Main Area
+        p Tabs: Skills, Combat, Spellcasting, Features, Equipment, Journal/Notes
+        p Welcome, {{ name}}
 </template>
 
 <script>
 export default {
   name: 'App',
   data: () => {
-   return {
-     name: "Colt"
-   }
- },
+    return {
+      name: "Colt"
+    }
+  }
 }
 </script>
